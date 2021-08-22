0fb09844cecb0760ece896e1ab443a1bad098d55
koylucan <84668234+koylucan@users.noreply.github.com>
2021-08-22 16:56:30 +0300

    Create 1409A - Yet Another Two Integers Problem.cpp

---
+++ 1409A - Yet Another Two Integers Problem.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+ 
+#define NOBOT ios::sync_with_stdio(0); cin.tie(0);
+#define FOR(i, l, n) for(int i = l; i < n; i++)
+#define RFOR(i, l, n) for(int i = l-1; i <= 0; i--)
+ 
+typedef long long ll;
+ 
+using namespace std;
+ 
+void solve(){
+	ll n, m;
+	cin>>n>>m;
+	
+	cout<<(abs(n-m)+9)/10<<endl<<endl;
+}
+ 
+int main(){
+	NOBOT;
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+		solve();
+	}
+	return 0;
+}

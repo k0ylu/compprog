01ea3100e554461e8609189d872598b857cb947a
koylucan <84668234+koylucan@users.noreply.github.com>
2021-10-30 21:37:44 +0300

    Create B. Shifting Sort.cpp

---
+++ B. Shifting Sort.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define NOBOT ios::sync_with_stdio(0); cin.tie(0);
+
+using ll = long long;
+using str = string;
+using db = long double;
+
+//pairs
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdb = pair<db, db>;
+
+#define mp_ make_pair
+#define fi first
+#define se second
+
+//vectors
+//i dont use them often tho
+using vii = vector<int, int>;
+using vll = vector<ll, ll>;
+using vdb = vector<db, db>;
+using vpii = vector<pii>;
+using vpdb = vector<pdb>;
+
+#define size(x) (int)(x.size())
+#define all(x) x.begin, x.end
+#define pb_ push_back
+
+void solve(){
+	int n;
+	cin>>n;
+	deque<int>ns;
+	
+	for(int i = 0; i < n; ++i){
+		int x; 
+		cin>>x;
+		
+		if(x <= ns[0]){
+			ns.push_front(x);
+		}
+		else{
+			ns.push_back(x);
+		}
+	}
+	
+	for(int i = 0; i < n; ++i){
+		cout<<ns[i]<<" ";
+	}
+	cout<<endl<<endl;	
+}
+		
+int main(){
+	NOBOT;
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+		solve();
+	}
+	return 0;
+}

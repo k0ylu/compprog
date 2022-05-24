bebd3f005b8b11749a6243fbb4d3f72c2fd05302
koylucan <pusat.eyigun@gmail.com>
2022-05-24 22:59:43 +0300

    vimrc #3
    -added a python keybind for later use
    -a new template

--- _vimrc
+++ _vimrc
@@ -5,9 +5,13 @@ set ts=4
 set sw=4
 set si
 set cb=unnamed
+" add a better font tbh
 
 autocmd filetype cpp nnoremap <F5> :w <bar> !g++ -std=c++17 % -o %:r<CR><CR>
 autocmd filetype cpp nnoremap <F6> :!%:r <CR><CR>
+
+autocmd filetype py nnoremap <F5> :w <bar> !py %:r <CR><CR>
+"^düzelt
 
 inoremap { {}<Left>
 inoremap {<CR> {<CR>}<Esc>O
---
+++ templates/tmain.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define NOBOT ios::sync_with_stdio(0); cin.tie(0);
+#define bsn "\n" //curse you turkish layout
+
+using ll = long long;
+using str = string;
+
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<double, double>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+#define SIN
+
+void solve(){
+
+}
+
+int main(){
+	NOBOT;	
+
+	#ifdef MUL
+	int t;
+	cin>>t;
+
+	while(t--){
+		solve();
+	}
+	#else
+	solve();
+	#endif
+
+	return 0;	
+}
+

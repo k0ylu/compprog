6a14fd3523fce56e777e52c533105c9bb3cae2ee
vcxndev <84668234+vcxndev@users.noreply.github.com>
2022-08-23 13:22:57 +0300

    Create Trailing Zeros.cxx

---
+++ CSES/Trailing Zeros.cxx
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define NOBOT ios::sync_with_stdio(0); cin.tie(0);
+#define bsn "\n" //curse you turkish layout
+
+#define mod 1000000007
+
+using ll = long long;
+using str = string;
+
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<double, double>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+void solve(){
+	int c;
+	cin >> c;
+	cout << (c-(c%5))/5 << bsn;
+  /*just find the multiples of 5*/
+}
+
+int main(){
+	NOBOT;	
+
+	int t;
+	cin>>t;
+
+	while(t--){
+		solve();
+	}
+
+	return 0;	
+}
+
